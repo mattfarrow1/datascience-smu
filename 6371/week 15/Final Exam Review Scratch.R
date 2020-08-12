f0f6649c707c28efa4f8e1ddfb73e450246374bf
Matt Farrow <matt@formalizedcuriosity.net>
2020-08-12 12:42:15 -0500

    update practice exam

--- 6371/week 15/Final Exam Review Scratch.R
+++ 6371/week 15/Final Exam Review Scratch.R
@@ -1,3 +1,19 @@
-options(scipen = 50)
+byo_anova <-
+  tibble(
+    "source" = c("Model", "Error", "Total"),
+    "df" = c(2, 393, 395),
+    "SS" = c(436.6, 14.51, 451.11),
+    "MS" = c(218.3, 0.0369, 0),
+    "F" = c(5915.9892, 0, 0),
+    "P-Value" = c(0, 0, 0)
+  )
 
-2e-16
+byo_anova[1,6] <- pf(byo_anova$F[1], byo_anova$df[2], byo_anova$df[1], lower.tail = FALSE)
+
+byo_anova <- na_if(byo_anova, 0)
+
+options(knitr.kable.NA = '')
+
+byo_anova %>% 
+  kable() %>% 
+  kable_styling("striped")
